7406a8337b539221573595f0aa9648f7fa5f3fc1
Jaydeepsinh Devda <djaybhai76@gmail.com>
2023-07-12 13:04:02 +0530

    full commit

---
+++ lib/main.dart
@@ -0,0 +1,174 @@
+import 'package:calculator_app/utils/colors.dart';
+import 'package:flutter/material.dart';
+import 'package:math_expressions/math_expressions.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter Demo',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(primarySwatch: Colors.blueGrey),
+        home: const CalculatorApp());
+  }
+}
+
+class CalculatorApp extends StatefulWidget {
+  const CalculatorApp({Key? key}) : super(key: key);
+
+  @override
+  State<CalculatorApp> createState() => _CalculatorAppState();
+}
+
+class _CalculatorAppState extends State<CalculatorApp> {
+  double firstNum = 0.0;
+  double secondNum = 0.0;
+  var input = '';
+  var output = '';
+  var operation = '';
+  var hideInput = false;
+  var outputSize = 34.0;
+
+  onButtonClick(value) {
+    if (value == "AC") {
+      input = '';
+      output = '';
+    } else if (value == "<") {
+      if (input.isNotEmpty) {
+        input = input.substring(0, input.length - 1);
+      }
+    } else if (value == "=") {
+      if (input.isNotEmpty) {
+        var userInput = input;
+        userInput = input.replaceAll("x", "*");
+        Parser p = Parser();
+        Expression expression = p.parse(userInput);
+        ContextModel cm = ContextModel();
+        var finalValue = expression.evaluate(EvaluationType.REAL, cm);
+        output = finalValue.toString();
+        if(output.endsWith(".0")){
+          output = output.substring(0, output.length - 2);
+        }
+        input = output;
+        hideInput = true;
+        outputSize = 52.0;
+      }
+    } else {
+      input = input + value;
+      hideInput = false;
+      outputSize = 34.0;
+    }
+
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.black,
+        body: Column(
+          children: [
+            // Input Output Area
+            Expanded(
+                child: Container(
+              width: double.infinity,
+              padding: const EdgeInsets.all(12),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Text(
+                    hideInput ? "" : input,
+                    style: const TextStyle(color: Colors.white, fontSize: 48),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Text(
+                    output,
+                    style: TextStyle(
+                        color: Colors.white.withOpacity(0.7), fontSize: outputSize),
+                  ),
+                  const SizedBox(height: 20),
+                ],
+              ),
+            )),
+
+            // Button Area
+            Row(
+              children: [
+                button(
+                    text: "AC",
+                    buttonBgColor: operatorColor,
+                    tColor: orangeColor),
+                button(
+                    text: "<",
+                    buttonBgColor: operatorColor,
+                    tColor: orangeColor),
+                button(text: "", buttonBgColor: Colors.transparent),
+                button(text: "/", buttonBgColor: operatorColor)
+              ],
+            ),
+            Row(
+              children: [
+                button(text: "7"),
+                button(text: "8"),
+                button(text: "9"),
+                button(text: "x", buttonBgColor: operatorColor)
+              ],
+            ),
+            Row(
+              children: [
+                button(text: "4"),
+                button(text: "5"),
+                button(text: "6"),
+                button(text: "-", buttonBgColor: operatorColor)
+              ],
+            ),
+            Row(
+              children: [
+                button(text: "1"),
+                button(text: "2"),
+                button(text: "3"),
+                button(text: "+", buttonBgColor: operatorColor)
+              ],
+            ),
+            Row(
+              children: [
+                button(text: "%"),
+                button(text: "0"),
+                button(text: "."),
+                button(text: "=", buttonBgColor: orangeColor)
+              ],
+            )
+          ],
+        ));
+  }
+
+  Widget button({text, tColor = Colors.white, buttonBgColor = buttonColor}) {
+    return Expanded(
+        child: Container(
+      margin: const EdgeInsets.all(8),
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+            padding: const EdgeInsets.all(22),
+            backgroundColor: buttonBgColor),
+        onPressed: () => onButtonClick(text),
+        child: Text(
+          text,
+          style: TextStyle(
+              color: tColor, fontWeight: FontWeight.bold, fontSize: 18),
+        ),
+      ),
+    ));
+  }
+}
