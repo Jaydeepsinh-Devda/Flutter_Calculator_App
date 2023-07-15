d4cca76364b6e633d2d7b588b76830ffca4be17c
Jaydeepsinh Devda <djaybhai76@gmail.com>
2023-07-15 11:39:10 +0530

    changes in lib folder

--- lib/main.dart
+++ lib/main.dart
@@ -28,11 +28,8 @@ class CalculatorApp extends StatefulWidget {
 }
 
 class _CalculatorAppState extends State<CalculatorApp> {
-  double firstNum = 0.0;
-  double secondNum = 0.0;
   var input = '';
   var output = '';
-  var operation = '';
   var hideInput = false;
   var outputSize = 34.0;
 
